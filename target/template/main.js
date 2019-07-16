cf5db5f3459256d2523bc032619971a693d9bec5
张宇 <runinspring@gmail.com>
2019-07-16 14:16:48 +0800

    support catch

--- target/template/main.js
+++ target/template/main.js
@@ -1,5 +1,15 @@
 require('./manifest.js');
 require('./egret.oppogame.js');
+
+// 启动本地缓存，如果开发者不需要此功能，只需注释即可
+// 只有使用 assetsmanager 的项目可以使用
+if(window.RES && RES.processor) {
+    require('./library/image.js');
+    require('./library/text.js');
+    require('./library/sound.js');
+    require('./library/binary.js');
+}
+
 egret.runEgret({
     //以下为自动修改，请勿修改
     //The following is automatically modified, please do not modify
