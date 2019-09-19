8387e3a99fa4badcd70ce591b8bb0c55c9924637
张宇 <runinspring@gmail.com>
2019-09-19 11:16:59 +0800

    update method convertImageToCanvas

--- src/WebTexture.ts
+++ src/WebTexture.ts
@@ -77,20 +77,9 @@ namespace egret.oppogame {
             }
             //从RenderTexture中读取像素数据，填入canvas
             let pixels = renderTexture.$renderBuffer.getPixels(rect.x, rect.y, iWidth, iHeight);
-            var x = 0;
-            var y = 0;
-            for (var i = 0; i < pixels.length; i += 4) {
-                sharedContext.fillStyle =
-                    'rgba(' + pixels[i]
-                    + ',' + pixels[i + 1]
-                    + ',' + pixels[i + 2]
-                    + ',' + (pixels[i + 3] / 255) + ')';
-                sharedContext.fillRect(x, y, 1, 1);
-                x++;
-                if (x == iWidth) {
-                    x = 0;
-                    y++;
-                }
+            let imageData = sharedContext.createImageData(iWidth,iHeight)
+            for (let i = 0; i < pixels.length; i++) {
+                imageData.data[i] = pixels[i];
             }
 
             if (!(<RenderTexture>texture).$renderBuffer) {
